530946d9ec823f80181b07d93c67561361f16e92
mdxs <van.wijgerden@mdxs.net>
2013-12-09 14:25:33 +0000

    Updated to Flask-WTF 0.9.3

--- main/lib/flaskext/wtf/form.py
+++ main/lib/flaskext/wtf/form.py
@@ -1,9 +1,21 @@
+# coding: utf-8
+
 import werkzeug.datastructures
 
 from jinja2 import Markup
 from flask import request, session, current_app
 from wtforms.fields import HiddenField
-from wtforms.ext.csrf.session import SessionSecureForm
+from wtforms.widgets import HiddenInput
+from wtforms.validators import ValidationError
+from wtforms.ext.csrf.form import SecureForm
+from ._compat import text_type, string_types
+from .csrf import generate_csrf, validate_csrf
+
+try:
+    from .i18n import translations
+except:
+    translations = None
+
 
 class _Auto():
     '''Placeholder for unspecified variables that should be set to defaults.
@@ -12,34 +24,48 @@ class _Auto():
     '''
     pass
 
-class Form(SessionSecureForm):
 
+def _is_hidden(field):
+    """Detect if the field is hidden."""
+    if isinstance(field, HiddenField):
+        return True
+    if isinstance(field.widget, HiddenInput):
+        return True
+    return False
+
+
+class Form(SecureForm):
     """
-    Flask-specific subclass of WTForms **SessionSecureForm** class.
+    Flask-specific subclass of WTForms **SecureForm** class.
 
-    Flask-specific behaviors:
-    If formdata is not specified, this will use flask.request.form. Explicitly
-      pass formdata = None to prevent this.
+    If formdata is not specified, this will use flask.request.form.
+    Explicitly pass formdata = None to prevent this.
 
-    csrf_context - a session or dict-like object to use when making CSRF tokens.
-      Default: flask.session.
+    :param csrf_context: a session or dict-like object to use when making
+                         CSRF tokens. Default: flask.session.
 
-    secret_key - a secret key for building CSRF tokens. If this isn't specified,
-      the form will take the first of these that is defined:
-        * the SECRET_KEY attribute on this class
-        * the value of flask.current_app.config["SECRET_KEY"]
-        * the session's secret_key
-      If none of these are set, raise an exception.
+    :param secret_key: a secret key for building CSRF tokens. If this isn't
+                       specified, the form will take the first of these
+                       that is defined:
 
-    csrf_enabled - whether to use CSRF protection. If False, all csrf behavior
-      is suppressed. Default: check app.config for CSRF_ENABLED, else True
+                       * SECRET_KEY attribute on this class
+                       * WTF_CSRF_SECRET_KEY config of flask app
+                       * SECRET_KEY config of flask app
+                       * session secret key
 
+    :param csrf_enabled: whether to use CSRF protection. If False, all
+                         csrf behavior is suppressed.
+                         Default: WTF_CSRF_ENABLED config value
     """
+    SECRET_KEY = None
+    TIME_LIMIT = 3600
+
     def __init__(self, formdata=_Auto, obj=None, prefix='', csrf_context=None,
                  secret_key=None, csrf_enabled=None, *args, **kwargs):
 
         if csrf_enabled is None:
-            csrf_enabled = current_app.config.get('CSRF_ENABLED', True)
+            csrf_enabled = current_app.config.get('WTF_CSRF_ENABLED', True)
+
         self.csrf_enabled = csrf_enabled
 
         if formdata is _Auto:
@@ -49,44 +75,51 @@ class Form(SessionSecureForm):
                     formdata = formdata.copy()
                     formdata.update(request.files)
                 elif request.json:
-                    formdata = werkzeug.datastructures.MultiDict(request.json);
+                    formdata = werkzeug.datastructures.MultiDict(request.json)
             else:
                 formdata = None
+
         if self.csrf_enabled:
             if csrf_context is None:
                 csrf_context = session
             if secret_key is None:
-                # It wasn't passed in, check if the class has a SECRET_KEY set
+                # It wasn't passed in, check if the class has a SECRET_KEY
                 secret_key = getattr(self, "SECRET_KEY", None)
-            if secret_key is None:
-                # It wasn't on the class, check the application config
-                secret_key = current_app.config.get("SECRET_KEY")
-            if secret_key is None and session:
-                # It's not there either! Is there a session secret key if we can
-                secret_key = session.secret_key
-            if secret_key is None:
-                # It wasn't anywhere. This is an error.
-                raise Exception('Must provide secret_key to use csrf.')
 
             self.SECRET_KEY = secret_key
         else:
             csrf_context = {}
-            self.SECRET_KEY = ""
-        super(Form, self).__init__(formdata, obj, prefix, csrf_context=csrf_context, *args, **kwargs)
+            self.SECRET_KEY = ''
+        super(Form, self).__init__(formdata, obj, prefix,
+                                   csrf_context=csrf_context,
+                                   *args, **kwargs)
 
     def generate_csrf_token(self, csrf_context=None):
         if not self.csrf_enabled:
             return None
-        return super(Form, self).generate_csrf_token(csrf_context)
+        return generate_csrf(self.SECRET_KEY, self.TIME_LIMIT)
 
     def validate_csrf_token(self, field):
         if not self.csrf_enabled:
-            return
-        super(Form, self).validate_csrf_token(field)
+            return True
+        if hasattr(request, 'csrf_valid') and request.csrf_valid:
+            # this is validated by CsrfProtect
+            return True
+        if not validate_csrf(field.data, self.SECRET_KEY, self.TIME_LIMIT):
+            raise ValidationError(field.gettext('CSRF token missing'))
+
+    def validate_csrf_data(self, data):
+        """Check if the csrf data is valid.
+
+        .. versionadded: 0.9.0
+
+        :param data: the csrf string to be validated.
+        """
+        return validate_csrf(data, self.SECRET_KEY, self.TIME_LIMIT)
 
     def is_submitted(self):
         """
-        Checks if form has been submitted. The default case is if the HTTP 
+        Checks if form has been submitted. The default case is if the HTTP
         method is **PUT** or **POST**.
         """
 
@@ -94,7 +127,7 @@ class Form(SessionSecureForm):
 
     def hidden_tag(self, *fields):
         """
-        Wraps hidden fields in a hidden DIV tag, in order to keep XHTML 
+        Wraps hidden fields in a hidden DIV tag, in order to keep XHTML
         compliance.
 
         .. versionadded:: 0.3
@@ -104,21 +137,25 @@ class Form(SessionSecureForm):
         """
 
         if not fields:
-            fields = [f for f in self if isinstance(f, HiddenField)]
+            fields = [f for f in self if _is_hidden(f)]
 
         rv = [u'<div style="display:none;">']
         for field in fields:
-            if isinstance(field, basestring):
+            if isinstance(field, string_types):
                 field = getattr(self, field)
-            rv.append(unicode(field))
+            rv.append(text_type(field))
         rv.append(u"</div>")
 
         return Markup(u"".join(rv))
-        
+
     def validate_on_submit(self):
         """
-        Checks if form has been submitted and if so runs validate. This is 
+        Checks if form has been submitted and if so runs validate. This is
         a shortcut, equivalent to ``form.is_submitted() and form.validate()``
         """
         return self.is_submitted() and self.validate()
-    
+
+    def _get_translations(self):
+        if not current_app.config.get('WTF_I18N_ENABLED', True):
+            return None
+        return translations
