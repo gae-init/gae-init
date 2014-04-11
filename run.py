ea64ae014552eae4d12e5b46fe0dda607bf4dd1b
mdxs <van.wijgerden@mdxs.net>
2014-04-12 01:15:35 +0200

    Moved update_missing_args closer to run method

--- run.py
+++ run.py
@@ -404,11 +404,6 @@ def print_out_update():
     pass
 
 
-def update_missing_args():
-  if ARGS.start or ARGS.clean_all:
-    ARGS.clean = True
-
-
 def uniq(seq):
   seen = set()
   return [e for e in seq if e not in seen and not seen.add(e)]
@@ -577,15 +572,20 @@ def run_start():
   os.system(run_command.replace('\n', ' '))
 
 
+def update_missing_args():
+  if ARGS.start or ARGS.clean_all:
+    ARGS.clean = True
+
+
 def run():
   if len(sys.argv) == 1:
     PARSER.print_help()
     sys.exit(1)
+  update_missing_args()
 
   os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
   update_path_separators()
-  update_missing_args()
 
   if ARGS.clean_all:
     run_clean_all()
