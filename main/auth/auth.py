bef8f0060528ef34a2840a7ade808da1e3e54fa7
David Stack <davidhstack@gmail.com>
2015-02-01 18:33:15 -0800

    Secured against timing attacks

--- main/auth/auth.py
+++ main/auth/auth.py
@@ -410,6 +410,6 @@ def get_user_db_from_email(email, password):
     return False
 
   user_db = user_dbs[0]
-  if user_db.password_hash == util.password_hash(user_db, password):
+  if util.constant_time_compare(user_db.password_hash, util.password_hash(password, user_db.password_salt)):
     return user_db
   return None
