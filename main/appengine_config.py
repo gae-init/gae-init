084b34cb599c2980e3fa10a7c384abb5340d9e6b
jake <web@jake.io>
2017-03-02 23:28:30 -0500

    clean up other venv references

--- main/appengine_config.py
+++ main/appengine_config.py
@@ -5,18 +5,7 @@ import sys
 
 from google.appengine.ext import vendor
 
-
-if os.environ.get('SERVER_SOFTWARE', '').startswith('Google App Engine'):
-  sys.path.insert(0, 'lib.zip')
-else:
-  if os.name == 'nt':
-    os.name = None
-    sys.platform = ''
-
-  vendor.add('venv', 0)
-
-sys.path.insert(0, 'libx')
-
+vendor.add('venv', 0)
 
 def webapp_add_wsgi_middleware(app):
   from google.appengine.ext.appstats import recording
