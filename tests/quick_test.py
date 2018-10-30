af1687c0731760f9e64207fbfaf84b227ac89308
Russ Bubley <russ.bubley@googlemail.com>
2018-10-30 16:50:17 +0000

    Style fixes

--- tests/quick_test.py
+++ tests/quick_test.py
@@ -1,30 +1,29 @@
-from webtest import TestApp
-import unittest
-from google.appengine.ext import testbed
-
 import sys
+import unittest
+
+from google.appengine.ext import testbed
+from webtest import TestApp
+
 sys.path.append('./main')
 
 
-
 class AppTest(unittest.TestCase):
-    def setUp(self):
-        # Wrap the app with WebTest's TestApp.
-        self.testbed = testbed.Testbed()
-        self.testbed.activate()
-        self.testbed.init_memcache_stub()
-        self.testbed.init_datastore_v3_stub()
-        from main import app
-        self.testapp = TestApp(app)
+  def setUp(self):
+    # Wrap the app with WebTest's TestApp.
+    self.testbed = testbed.Testbed()
+    self.testbed.activate()
+    self.testbed.init_memcache_stub()
+    self.testbed.init_datastore_v3_stub()
+    from main import app
+    self.testapp = TestApp(app)
 
+  def tearDown(self):
+    self.testbed.deactivate()
 
-    def tearDown(self):
-        self.testbed.deactivate()
+  def testIndexHandler(self):
+    response = self.testapp.get('/')
+    self.assertEqual(response.status_int, 200)
 
-    def testIndexHandler(self):
-        response = self.testapp.get('/')
-        self.assertEqual(response.status_int, 200)
-
-    def testNonExistentHandler(self):
-        response = self.testapp.get('/not-there', status=404)
-        self.assertEqual(response.status_int, 404)
+  def testNonExistentHandler(self):
+    response = self.testapp.get('/not-there', status=404)
+    self.assertEqual(response.status_int, 404)
