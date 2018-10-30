4f2c0b03e8c4034de12da0fb196301a5be45fce7
Russ Bubley <russ.bubley@googlemail.com>
2018-10-30 11:38:40 +0000

    Add functional test suite

---
+++ tests/quick_test.py
@@ -0,0 +1,29 @@
+from webtest import TestApp
+import unittest
+from google.appengine.ext import testbed
+
+import sys
+sys.path.append('./main')
+
+
+
+class AppTest(unittest.TestCase):
+    def setUp(self):
+        # Wrap the app with WebTest's TestApp.
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_all_stubs()
+        from main import app
+        self.testapp = TestApp(app)
+
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def testIndexHandler(self):
+        response = self.testapp.get('/')
+        self.assertEqual(response.status_int, 200)
+
+    def testNonExistentHandler(self):
+        response = self.testapp.get('/not-there', status=404)
+        self.assertEqual(response.status_int, 404)
