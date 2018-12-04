865752dbee5aa04d16bb6855861c38e48e86b852
nathan lloyd <nathan.lloyd@impossiblefoods.com>
2018-12-04 10:38:00 -0800

    Remove testing that involves google.appengine.ext.testbed from quick_test.py.

--- tests/quick_test.py
+++ tests/quick_test.py
@@ -1,7 +1,6 @@
 import sys
 import unittest
 
-from google.appengine.ext import testbed
 from webtest import TestApp
 
 sys.path.append('./main')
@@ -10,15 +9,8 @@ sys.path.append('./main')
 class AppTest(unittest.TestCase):
   def setUp(self):
     # Wrap the app with WebTest's TestApp.
-    self.testbed = testbed.Testbed()
-    self.testbed.activate()
-    self.testbed.init_memcache_stub()
-    self.testbed.init_datastore_v3_stub()
     from main import app
     self.testapp = TestApp(app)
-
-  def tearDown(self):
-    self.testbed.deactivate()
 
   def test_index_handler(self):
     response = self.testapp.get('/')
