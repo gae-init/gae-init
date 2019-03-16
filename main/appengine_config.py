bd5c0eb61e1329563487bb8dd169e89326d0b14c
brubakerjeff <brubaker.jeff@gmail.com>
2019-03-16 13:26:38 -0400

    updated comment

--- main/appengine_config.py
+++ main/appengine_config.py
@@ -6,9 +6,10 @@ import sys
 from path_util import sys_path_insert
 
 # lib.zip has been precompiled from the gulp tasks when running inside of the Google Environment 
-# otherwise compile it
+# in the first case use the copy of lib.zip that has been pre-compiled
 if os.environ.get('SERVER_SOFTWARE', '').startswith('Google App Engine'):
   sys_path_insert('lib.zip')
+# in the case when running locally compile lib.zip on demand
 else:
   if os.name == 'nt':
     os.name = None
