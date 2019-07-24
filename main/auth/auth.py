6599b2604c92c39748e8bc56e04f9b403fb9ce2a
Russ Bubley <russ.bubley@googlemail.com>
2019-07-24 16:00:56 +0100

    Migrate OAuthException -> OAuthError

--- main/auth/auth.py
+++ main/auth/auth.py
@@ -350,7 +350,7 @@ def signin_oauth(oauth_app, scheme=None):
     return oauth_app.authorize_redirect(flask.url_for(
       '%s_authorized' % oauth_app.name, _external=True, _scheme=scheme
     ))
-  except oauth.OAuthException:
+  except oauth.OAuthError:
     flask.flash(
       'Something went wrong with sign in. Please try again.',
       category='danger',
