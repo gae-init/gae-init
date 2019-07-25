9bf9ca7a6023b2003498e47ccc22c04a42bd9922
Lipis <lipiridis@gmail.com>
2019-07-25 13:50:43 +0300

    Remove bunch of OAuth logins (#1215)

--- main/auth/__init__.py
+++ main/auth/__init__.py
@@ -1,18 +1,11 @@
 # coding: utf-8
 
 from .auth import *
-from .azure_ad import *
 from .bitbucket import *
-from .dropbox import *
 from .facebook import *
 from .github import *
 from .gae import *
 from .google import *
 from .instagram import *
-from .linkedin import *
-from .mailru import *
 from .microsoft import *
-from .reddit import *
 from .twitter import *
-from .vk import *
-from .yahoo import *
