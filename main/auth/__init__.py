527e8ca1ea2ef7869bda555ce382da46410a9c3b
Russ Bubley <russ.bubley@googlemail.com>
2020-03-01 22:21:36 +0000

    Merge

--- main/auth/__init__.py
+++ main/auth/__init__.py
@@ -4,7 +4,6 @@ from .auth import *
 from .bitbucket import *
 from .facebook import *
 from .github import *
-from .gae import *
 from .google import *
 from .microsoft import *
 from .twitter import *
