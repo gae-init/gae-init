77e42b102c710acb306b57f4775ce6ce2e4e7f4a
mdxs <van.wijgerden@mdxs.net>
2020-03-10 10:54:53 +0100

    Add error handler for 409 - Conflict

--- main/control/error.py
+++ main/control/error.py
@@ -16,6 +16,7 @@ from main import app
 @app.errorhandler(403)  # Forbidden
 @app.errorhandler(404)  # Not Found
 @app.errorhandler(405)  # Method Not Allowed
+@app.errorhandler(409)  # Conflict
 @app.errorhandler(410)  # Gone
 @app.errorhandler(418)  # I'm a Teapot
 @app.errorhandler(422)  # Unprocessable Entity
