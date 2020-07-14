6a0d0b35e5e7ea6dbe6f044859a85aca2016ac41
Russ Bubley <russ.bubley@googlemail.com>
2020-07-14 18:35:45 +0100

    Narrow down problem a bit

--- main/auth/twitch.py
+++ main/auth/twitch.py
@@ -20,22 +20,30 @@ twitch_config = dict(
   client_secret=config.CONFIG_DB.twitch_client_secret,
   request_token_params={
     'scope': 'user:read:email',
-    'state': util.uuid(),
     'token_endpoint_auth_method': 'client_secret_post',
+		"Client-ID": config.CONFIG_DB.twitch_client_id,
   },
 )
 
 twitch = auth.create_oauth_app(twitch_config, 'twitch')
 
+def twitch_compliance_fix(url, headers, data):
+  headers["Client-ID"] = config.CONFIG_DB.twitch_client_id
+  return url, headers, data
+
+twitch.pre_request = twitch_compliance_fix
 
 @app.route('/api/auth/callback/twitch/')
 def twitch_authorized():
+  import logging
+  logging.info('####### %r' % twitch.token)
   id_token = twitch.authorize_access_token()
+  logging.info('####### %r' % twitch.token)
   if id_token is None:
     flask.flash('You denied the request to sign in.')
     return flask.redirect(util.get_next_url())
 
-  me = twitch.get('user')  # Need to change this line
+  me = twitch.get('user')
   user_db = retrieve_user_from_twitch(me.json())
   return auth.signin_user_db(user_db)
 
