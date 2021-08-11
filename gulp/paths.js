76d7a4762b1e3f2329478990602258a48603bab5
mdxs <van.wijgerden@mdxs.net>
2021-08-12 00:37:22 +0200

    Improved cleanup in development

--- gulp/paths.js
+++ gulp/paths.js
@@ -19,8 +19,8 @@
   paths.dep.py_guard = `${paths.temp.root}/pip.guard`;
 
   paths.py.lib = `${paths.main}/lib`;
-  paths.py.lib_file = `${paths.py.lib}.zip`;
-
+  paths.py.libdev = `${paths.main}/libdev`;
+  
   paths.static.root = `${paths.main}/static`;
   paths.static.dev = `${paths.static.root}/dev`;
   paths.static.ext = `${paths.static.root}/ext`;
