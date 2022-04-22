3e6e2aad03c2da78067692a4c451675c1399af09
mdxs <van.wijgerden@mdxs.net>
2022-04-22 18:26:08 +0200

    Removed superfluous import (#1722)

--- main/appengine_config.py
+++ main/appengine_config.py
@@ -1,16 +1,16 @@
 # coding: utf-8
+
 import os
 
 from google.appengine.ext import vendor
 
 if not os.environ.get('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
   import sys
+
   path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'libdev')
   sys.path.insert(0, path)
 
   if os.name == 'nt':
-    import sys
-
     os.name = None
     sys.platform = ''
 
